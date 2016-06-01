b043e23e7d1bb01a3963384791336fbfe55ffef0
PatrykM <patryk.molik@hpe.com>
2016-06-01 14:09:53 +0200

    First working version

---
+++ MainForm.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace The_Renamer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkFoundItems = new System.Windows.Forms.CheckedListBox();
+            this.btnFiles = new System.Windows.Forms.Button();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtReplace = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtWhere = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chkFoundItems
+            // 
+            this.chkFoundItems.CheckOnClick = true;
+            this.chkFoundItems.FormattingEnabled = true;
+            this.chkFoundItems.Location = new System.Drawing.Point(12, 166);
+            this.chkFoundItems.Name = "chkFoundItems";
+            this.chkFoundItems.Size = new System.Drawing.Size(590, 304);
+            this.chkFoundItems.TabIndex = 0;
+            this.chkFoundItems.SelectedIndexChanged += new System.EventHandler(this.chkFoundItems_SelectedIndexChanged);
+            // 
+            // btnFiles
+            // 
+            this.btnFiles.Location = new System.Drawing.Point(26, 63);
+            this.btnFiles.Name = "btnFiles";
+            this.btnFiles.Size = new System.Drawing.Size(75, 23);
+            this.btnFiles.TabIndex = 1;
+            this.btnFiles.Text = "Files";
+            this.btnFiles.UseVisualStyleBackColor = true;
+            this.btnFiles.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtFind
+            // 
+            this.txtFind.Location = new System.Drawing.Point(26, 37);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(167, 20);
+            this.txtFind.TabIndex = 2;
+            this.txtFind.Text = "test";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(118, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Folders";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtReplace
+            // 
+            this.txtReplace.Location = new System.Drawing.Point(245, 37);
+            this.txtReplace.Name = "txtReplace";
+            this.txtReplace.Size = new System.Drawing.Size(150, 20);
+            this.txtReplace.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Find";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(242, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Replace with";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(245, 63);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Replace";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(527, 111);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Browse";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Where";
+            // 
+            // txtWhere
+            // 
+            this.txtWhere.Enabled = false;
+            this.txtWhere.Location = new System.Drawing.Point(26, 111);
+            this.txtWhere.Name = "txtWhere";
+            this.txtWhere.Size = new System.Drawing.Size(495, 20);
+            this.txtWhere.TabIndex = 8;
+            this.txtWhere.Text = "C:\\Users\\molik\\Desktop\\test";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(26, 137);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "Check all";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(107, 137);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "Uncheck all";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 484);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtWhere);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtReplace);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtFind);
+            this.Controls.Add(this.btnFiles);
+            this.Controls.Add(this.chkFoundItems);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "The Renamer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox chkFoundItems;
+        private System.Windows.Forms.Button btnFiles;
+        private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtReplace;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtWhere;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The_Renamer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        { InitializeComponent(); }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.chkFoundItems.Items.Clear();
+            string[] files = Directory.GetFiles(this.txtWhere.Text, "*" + this.txtFind.Text + "*", SearchOption.AllDirectories);
+            if (files.Count() == 0)
+            {
+                MessageBox.Show("No Items found");
+            }
+            else
+            {
+                //this.chkFoundItems.Items.Add("*All");
+                this.chkFoundItems.Items.AddRange(files);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            { this.txtWhere.Text = folderBrowserDialog.SelectedPath; }
+        }
+
+        private void chkFoundItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //if (this.chkFoundItems.CheckedItems.Count == 1 && this.chkFoundItems.CheckedItems(0) = )
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.chkFoundItems.Items.Count; i++)
+            { this.chkFoundItems.SetItemChecked(i, true); }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            foreach (int i in this.chkFoundItems.CheckedIndices)
+            { this.chkFoundItems.SetItemCheckState(i, CheckState.Unchecked); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.chkFoundItems.Items.Clear();
+            string[] files = Directory.GetDirectories(this.txtWhere.Text, "*" + this.txtFind.Text + "*", SearchOption.AllDirectories);
+            if (files.Count() == 0)
+            { MessageBox.Show("No Items found"); }
+            else
+            {
+                //this.chkFoundItems.Items.Add("*All");
+                this.chkFoundItems.Items.AddRange(files);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (string s in this.chkFoundItems.CheckedItems)
+            { 
+                DirectoryInfo di = new DirectoryInfo(s);
+                string oldName = di.Name;
+                string newName = oldName.Replace(this.txtFind.Text, this.txtReplace.Text);
+                RenameTo(di, newName); 
+            }
+            this.chkFoundItems.Items.Clear();
+            MessageBox.Show("Finished");
+        }
+        //private DirectoryInfo DirectoryInfo(string s)
+        //{
+        //    throw new NotImplementedException();
+        //}
+        public static void RenameTo(DirectoryInfo di, string newName)//this direct
+        {
+            if (di == null)
+            { throw new ArgumentNullException("di", "Directory info to rename cannot be null"); }
+
+            if (string.IsNullOrWhiteSpace(newName))
+            { throw new ArgumentException("Old string cannot be null or blank", "Chars"); }
+
+            string oldPath = di.Parent.FullName;
+            di.MoveTo(Path.Combine(oldPath, newName));
+
+            //return; //done
+        }
+    }
+}
